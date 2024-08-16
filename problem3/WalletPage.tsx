9bc072b4b18fbc0cea1cefc7ba79dd52f85caa6c
Yeami <ivanov.nikita@yeami.group>
2024-08-16 17:28:56 +0300

    Refactor the code and add comments

--- problem3/WalletPage.tsx
+++ problem3/WalletPage.tsx
@@ -1,81 +1,75 @@
+// Create separate enum of all supported blockchains
+enum Blockchain {
+  Osmosis = 'Osmosis',
+  Ethereum = 'Ethereum',
+  Arbitrum = 'Arbitrum',
+  Zilliqa = 'Zilliqa',
+  Neo = 'Neo'
+}
+
 interface WalletBalance {
   currency: string;
   amount: number;
-}
-interface FormattedWalletBalance {
-  currency: string;
-  amount: number;
-  formatted: string;
+  blockchain: Blockchain | string; // Add the blockchain field (It was used in the code, but was not presented in interface)
 }
 
-interface Props extends BoxProps {
+// Remove FormattedWalletBalance
 
-}
+interface Props extends BoxProps {}
+
+// Create list of all blockchains with priorities
+const blockchainPriority: Record<Blockchain, number> = {
+  [Blockchain.Osmosis]: 100,
+  [Blockchain.Ethereum]: 50,
+  [Blockchain.Arbitrum]: 30,
+  [Blockchain.Zilliqa]: 20,
+  [Blockchain.Neo]: 20,
+};
+
 const WalletPage: React.FC<Props> = (props: Props) => {
-  const { children, ...rest } = props;
+  // Remove 'children' prop because it was not used
+  // Replace the '...rest' props with specific props, because it will make code more readable and understandable
+  const { className, style } = props;
+
   const balances = useWalletBalances();
   const prices = usePrices();
 
-	const getPriority = (blockchain: any): number => {
-	  switch (blockchain) {
-	    case 'Osmosis':
-	      return 100
-	    case 'Ethereum':
-	      return 50
-	    case 'Arbitrum':
-	      return 30
-	    case 'Zilliqa':
-	      return 20
-	    case 'Neo':
-	      return 20
-	    default:
-	      return -99
-	  }
-	}
+  // Get priority from list Instead of switch-case. It will be easily to expand the list
+  const getPriority = (blockchain: Blockchain | string): number => { // Allow string as param is case if unsupported blockchain name was passed
+    return blockchainPriority[blockchain as Blockchain] || -99;
+  };
 
+  // It will be better to not use the useMemo if balances will be updated frequently
   const sortedBalances = useMemo(() => {
-    return balances.filter((balance: WalletBalance) => {
-		  const balancePriority = getPriority(balance.blockchain);
-		  if (lhsPriority > -99) {
-		     if (balance.amount <= 0) {
-		       return true;
-		     }
-		  }
-		  return false
-		}).sort((lhs: WalletBalance, rhs: WalletBalance) => {
-			const leftPriority = getPriority(lhs.blockchain);
-		  const rightPriority = getPriority(rhs.blockchain);
-		  if (leftPriority > rightPriority) {
-		    return -1;
-		  } else if (rightPriority > leftPriority) {
-		    return 1;
-		  }
-    });
-  }, [balances, prices]);
+    return balances
+      .filter((balance: WalletBalance) => getPriority(balance.blockchain) > -99 && balance.amount > 0)
+      .sort((balanceA: WalletBalance, balanceB: WalletBalance) => { // lhs and rhs are bad names, better to use names that will improve code clarity
+        const priorityA = getPriority(balanceA.blockchain);
+        const priorityB = getPriority(balanceB.blockchain);
+        return priorityB - priorityA;
+      });
+  }, [balances]); // Remove prices dependency
 
-  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
-    return {
-      ...balance,
-      formatted: balance.amount.toFixed()
-    }
-  })
-
-  const rows = sortedBalances.map((balance: FormattedWalletBalance, index: number) => {
+  const rows = sortedBalances.map((balance: WalletBalance, index: number) => {
+    const formattedAmount = balance.amount.toFixed(2); // Better to get the formatted amount here instead of using .map() twice
     const usdValue = prices[balance.currency] * balance.amount;
+
+    // Remove the className prop because we do not have any classes variable in code
     return (
       <WalletRow
-        className={classes.row}
         key={index}
         amount={balance.amount}
         usdValue={usdValue}
-        formattedAmount={balance.formatted}
+        formattedAmount={formattedAmount}
       />
-    )
-  })
+    );
+  });
 
-  return (
-    <div {...rest}>
-      {rows}
-    </div>
-  )
-}
+  // Passing each prop directly instead of {...rest} will increase the code readability
+  return <div
+      className={className}
+      style={style}
+  >
+    {rows}
+  </div>;
+};
