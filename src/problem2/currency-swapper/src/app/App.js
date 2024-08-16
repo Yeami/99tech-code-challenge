258018d9bd37d50b083cbec770ed7b45ea92c436
Yeami <ivanov.nikita@yeami.group>
2024-08-16 04:07:14 +0300

    Minor fixes after styles file change and remove comments

--- src/problem2/currency-swapper/src/app/App.js
+++ src/problem2/currency-swapper/src/app/App.js
@@ -1,26 +1,12 @@
 import logo from '../assets/logo.svg';
-import './App.css';
-import CurrencyConverter from '../components/CurrencyConverter';
+import styles from './App.module.scss'
+import CurrencyConverter from '../components/currency-converter';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} alt="Logo" />
+    <div className={styles['app']}>
+      <img src={logo} className={styles['logo']} alt="Logo" />
       <CurrencyConverter/>
-      {/*<header className="App-header">*/}
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*<p>*/}
-        {/*  Edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
-      {/*</header>*/}
     </div>
   );
 }
